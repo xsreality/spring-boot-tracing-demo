e9f93113137892f9bd2db44044a6659bfa6d6562
Abhinav Sonkar <xsabhinav@gmail.com>
2021-10-17 00:27:08 +0200

    Modify application for tracing: service1 --> Kafka --> service2

--- service1/src/main/java/com/example/service1/TracingProducerConfig.java
+++ service1/src/main/java/com/example/service1/TracingProducerConfig.java
@@ -1,5 +1,6 @@
-package com.example.service2;
+package com.example.service1;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +12,6 @@ import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG;
 
 @EnableKafka
 @Configuration
@@ -27,7 +26,7 @@ public class TracingProducerConfig {
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
 
-        props.put(CLIENT_ID_CONFIG, "com.example.tracing-demo-producer");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "com.example.tracing-demo-producer");
         props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass().getName());
         props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass().getName());
 
--- service2/src/main/java/com/example/service2/TracingConsumer.java
+++ service2/src/main/java/com/example/service2/TracingConsumer.java
@@ -1,8 +1,6 @@
 package com.example.service2;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.sleuth.Span;
@@ -11,47 +9,22 @@ import org.springframework.cloud.sleuth.instrument.kafka.TracingKafkaPropagatorG
 import org.springframework.cloud.sleuth.propagation.Propagator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.ProducerListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@RestController
-public class HomeController {
-    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+public class TracingConsumer {
+    private static final Logger log = LoggerFactory.getLogger(TracingConsumer.class);
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
     private final RestTemplate restTemplate;
     private final Tracer tracer;
     private final Propagator propagator;
 
-    public HomeController(KafkaTemplate<String, String> kafkaTemplate, RestTemplate restTemplate, Tracer tracer, Propagator propagator) {
-        this.kafkaTemplate = kafkaTemplate;
+    public TracingConsumer(RestTemplate restTemplate, Tracer tracer, Propagator propagator) {
         this.restTemplate = restTemplate;
         this.tracer = tracer;
         this.propagator = propagator;
-    }
-
-    @RequestMapping("/")
-    String service2() {
-        log.info("Hit service 2!");
-        this.kafkaTemplate.setProducerListener(new ProducerListener<>() {
-            @Override
-            public void onSuccess(ProducerRecord<String, String> producerRecord, RecordMetadata recordMetadata) {
-                log.info("Successfully updated kafka {}", producerRecord.value());
-            }
-
-            @Override
-            public void onError(ProducerRecord<String, String> producerRecord, RecordMetadata metadata, Exception exception) {
-                log.error("Error producing record {}", producerRecord, exception);
-            }
-        });
-        this.kafkaTemplate.send("tracing-demo", "this is key", "this is value at " + System.currentTimeMillis());
-        return "Service 2 responding for duty.";
     }
 
     @KafkaListener(topics = "tracing-demo")
