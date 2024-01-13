5e33de731472a3d94e02589ec89acf3e4bbc00b1
Abhinav Sonkar <xsabhinav@gmail.com>
2024-01-13 15:56:25 +0100

    Upgrade to Spring Boot 3 with Micrometer

--- otel-http/httpservice1/src/main/java/com/example/HttpService1Application.java
+++ otel-http/httpservice1/src/main/java/com/example/HttpService1Application.java
@@ -1,18 +1,58 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HttpService1Application {
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(HttpService1Application.class, args);
+    }
+}
+
+@Service
+class MyService {
+
+    private final RestTemplate restTemplate;
+
+
+    MyService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    String callService2() {
+        return this.restTemplate.getForObject("http://localhost:8081", String.class);
+    }
+}
+
+@RestController
+class HomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    private final MyService service;
+
+    HomeController(MyService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/")
+    String home() {
+        log.info("Hit service 2");
+        return service.callService2();
     }
 }
--- otel-http/httpservice2/src/main/java/com/example/HttpService2Application.java
+++ otel-http/httpservice2/src/main/java/com/example/HttpService2Application.java
@@ -1,18 +1,58 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HttpService2Application {
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(HttpService2Application.class, args);
+    }
+}
+
+@Service
+class FetchCommitService {
+
+    private static final Logger log = LoggerFactory.getLogger(FetchCommitService.class);
+
+    private final RestTemplate restTemplate;
+
+    FetchCommitService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    String fetchCommit() {
+        String commitMsg = this.restTemplate.getForObject("https://whatthecommit.com/index.txt", String.class);
+        log.info(commitMsg);
+        return commitMsg;
+    }
+}
+
+@RestController
+class HomeController {
+
+    private final FetchCommitService service;
+
+    HomeController(FetchCommitService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return service.fetchCommit();
     }
 }
