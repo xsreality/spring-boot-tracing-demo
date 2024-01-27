488052be161b4dc96a1697c40c1f811857579d8d
Abhinav Sonkar <xsabhinav@gmail.com>
2024-01-27 21:13:34 +0100

    Demonstrate micrometer integration test

--- otel-http-micrometer/httpservice2-micrometer/src/main/java/com/example/HttpService2Application.java
+++ otel-http-micrometer/httpservice2-micrometer/src/main/java/com/example/HttpService2Application.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -32,6 +33,9 @@ class FetchCommitService {
 
     private static final Logger log = LoggerFactory.getLogger(FetchCommitService.class);
 
+    @Value("${whatthecommit.url}")
+    private String whatTheCommitUrl;
+
     private final RestTemplate restTemplate;
     private final ObservationRegistry registry;
 
@@ -43,7 +47,7 @@ class FetchCommitService {
     String fetchCommit() {
         var observation = Observation.createNotStarted("fetch-commit", registry).start();
         try (var ignored = observation.openScope()) {
-            String commitMsg = this.restTemplate.getForObject("https://whatthecommit.com/index.txt", String.class);
+            String commitMsg = this.restTemplate.getForObject(whatTheCommitUrl, String.class);
             //noinspection DataFlowIssue
             observation.highCardinalityKeyValue("commit.message", commitMsg);
             observation.event(Observation.Event.of("commit-fetched"));
